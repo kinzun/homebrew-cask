c1f9bc59a9615246e7444d1c363ecec231c0a391
kinzun <baolei@protonmail.com>
2018-12-28 21:24:27 +0800

    Update rightfont.rb
    57b955fc1ef48284a8ac654f73cb3bb08c1ae4fec265792a508896f64c012e9f 
    replace new sha256
    "c5e748815d31c88cc968fe681fb40cec3dc31fe56441cd794c1bf26b8e7257eb"

--- Casks/rightfont.rb
+++ Casks/rightfont.rb
@@ -1,6 +1,6 @@
 cask 'rightfont' do
   version '5.2.4'
-  sha256 '57b955fc1ef48284a8ac654f73cb3bb08c1ae4fec265792a508896f64c012e9f'
+  sha256 'c5e748815d31c88cc968fe681fb40cec3dc31fe56441cd794c1bf26b8e7257eb'
 
   url 'https://rightfontapp.com/update/rightfont.zip'
   appcast "https://rightfontapp.com/update/appcast#{version.major}.xml"
